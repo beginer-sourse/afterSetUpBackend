ca8978c98ce3798e0cd892a74aaff7f0b8259023
beginer-sourse <piyushkingyadav@gmail.com>
2025-08-20 23:53:28 +0530

    added routes of avtar,coverImage,userInfo,currentUser

--- setUpBackend/src/routes/user.route.js
+++ setUpBackend/src/routes/user.route.js
@@ -1,5 +1,8 @@
 import { Router } from "express";
-import { loginUser, logOutUser, refreshAccessToken, registerUser } from "../controllers/user.controller.js";
+
+import { getCurrnetUser, loginUser, logOutUser, passwordUpdate, refreshAccessToken, 
+  registerUser, updateUserInfo , updateAvtarInfo ,updateCoverInfo} from "../controllers/user.controller.js";
+
 import {upload} from "../middlewares/multer.js"
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -22,8 +25,15 @@ router.route("/register").post(
 router.route("/logIn").post(loginUser)
 
 // secure route because token are verified in it.
-router.route("/logOut").post(verifyJWT,logOutUser)
 
+router.route("/logOut").post(verifyJWT,logOutUser)
 router.route("/refreshToken").post(refreshAccessToken)
+router.route("/passwordChange").post(verifyJWT,passwordUpdate)
+router.route("/currentUser").get(verifyJWT, getCurrnetUser)
+router.route("/updateUserInfo").post(verifyJWT, updateUserInfo)
+router.route("/updateAvatarInfo").post(verifyJWT,upload.single("UpdateAvatar"),updateAvtarInfo)
+router.route("/updateCoverInfo").post(verifyJWT,upload.single("UpdateCoverImage"),updateCoverInfo)
+
+
 
 export default router; // you can give any name of your choice in import if export if default.
